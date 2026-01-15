ebd85f64298d35c70c6f169f394f57f0afaf052b
Matias Arquero <matiasarquerochamorro@gmail.com>
2026-01-15 18:57:56 -0300

    Menu principal estilizado

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,9 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('')
+    return render_template('index.html')
+
+
+@app.route('/ejercicio1.html')
+def ejercicio1():
+    return render_template('ejercicio1.html')
