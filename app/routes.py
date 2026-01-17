826f6b186df436b01498315711503ca8e9f499ac
Matias Arquero <matiasarquerochamorro@gmail.com>
2026-01-16 21:33:44 -0300

    Ej1 logica lista

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
@@ -7,6 +7,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/ejercicio1.html')
+@app.route('/ejercicio1', methods=['GET', 'POST'])
 def ejercicio1():
-    return render_template('ejercicio1.html')
+    promedio = None
+    asistencia = None
+
+    if request.method == 'POST':
+        nota1 = int(request.form['nota1'])
+        nota2 = int(request.form['nota2'])
+        nota3 = int(request.form['nota3'])
+        asistencia = float(request.form['asistencia'])
+
+        promedio = (nota1 + nota2 + nota3) / 3
+
+    return render_template('ejercicio1.html',p=promedio, a=asistencia
